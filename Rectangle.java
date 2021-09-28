d05b2f39a5c60aba8915616978122b19cc6364e1
om21sanket50326 <91517187+om21sanket50326@users.noreply.github.com>
2021-09-28 12:32:57 +0530

    b2 uploded
    uploded by b2

---
+++ Rectangle.java
@@ -0,0 +1,31 @@
+
+
+//Q 8 Take values of length and breadth of a rectangle from user and check if it is square or not.
+//hint is length==breath square else rectangle 
+
+
+
+package java_assignments;
+
+public class Rectangle {
+
+	public static void main(String[] args) {
+
+		int length = 40;
+		int breadth = 22;
+		
+		if(length == breadth)
+		{
+			System.out.println("It is square");
+		}
+		
+		else 
+		{
+			System.out.println("It is Rectangle");
+		}
+
+
+
+	}
+
+}
