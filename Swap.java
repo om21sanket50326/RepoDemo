d20f310f233e936c90921ea54359fed0613ec98b
om21sanket50326 <91517187+om21sanket50326@users.noreply.github.com>
2021-09-28 12:27:25 +0530

    1st b1uploaded file
    1st b1 uploadation

---
+++ Swap.java
@@ -0,0 +1,26 @@
+
+
+//Q 2  Wap to swap two numbers with third variable
+
+
+package java_assignments;
+
+public class Swap {
+
+	public static void main(String[] args)
+	{
+
+
+		int a=1;
+		int b=2;
+		int c;
+		 System.out.println("before swapping digit \n a="+a+" \n b="+b);
+		 
+		  c=a;
+		  a=b;
+		  b=c;
+		System.out.println("after swapping digit \n a="+a+" \n b="+b);
+
+	}
+
+}
